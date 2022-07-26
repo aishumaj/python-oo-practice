f569c84822eba3885e9bceae42179a22469fddc9
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-07-26 16:05:29 -0700

    completed wordfinder class exercise

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,33 @@
+from random import choice
+
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
+    """Word Finder: finds random words from a dictionary.
+
+        >>> words = WordFinder(filepath = /Users/aishu_maj/Desktop/words.txt)
+        10
+    """
+    def __init__(self, filepath):
+        """Creates instance that reads a filepath with a list of words and
+        reports the number of words read"""
+        self.filepath = filepath
+        self.words = []
+        self.read_file_and_create_list()
+        self.print_words_read()
+
+    def __repr__(self):
+        return f"This WordFinder instance contains filepath {self.filepath}"
+
+    def read_file_and_create_list(self):
+        """Reads filepath, populating word list with words from the file"""
+        file = open(self.filepath)
+        for line in file:
+            self.words.append(line)
+        return self.words
+
+    def print_words_read(self):
+        """Prints the number of words in file"""
+        print(f"{len(self.words)} words read")
+
+    def random(self):
+        """Returns a random word from word list"""
+        return choice(self.words).replace("\n", "")
