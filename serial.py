e0d3dc6b6ad4af9022e85d0a3026fa0273d6ea46
Andrew Jensen <andrewkjensen@gmail.com>
2022-07-26 15:32:47 -0700

    make serial.py functional on the first try

--- serial.py
+++ serial.py
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """create serial number with starting number"""
+        self.start = start
+        self.initial_start = start
+
+    def __repr__(self):
+        return f"SerialGenerator start = {self.start}"
+
+    def generate(self):
+        """on the first call, return the start value.
+            On later calls, add one to previos serial number and return it"""
+        self.start += 1
+        return self.start - 1
+
+    def reset(self):
+        """reset the serial number to the start value and return None"""
+        self.start = self.initial_start
+    
